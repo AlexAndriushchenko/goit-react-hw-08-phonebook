ff30143f821a0320b3462b4a24a28f381496191f
AlexAndriushchenko <102898333+AlexAndriushchenko@users.noreply.github.com>
2023-05-18 19:01:25 +0000

    Deploying to gh-pages from @ AlexAndriushchenko/goit-react-hw-08-phonebook@b7a7b192ad2e18da0af22823247328f7ac9fd663 🚀

--- static/js/266.b76a22ec.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[266],{1266:function(e,t,n){n.r(t),n.d(t,{default:function(){return c}});var i=n(3329),o={container:{minHeight:"calc(100vh - 50px)",display:"flex",alignItems:"center",justifyContent:"center"},title:{fontWeight:500,fontSize:48,textAlign:"center"}};function c(){return(0,i.jsx)("div",{style:o.container,children:(0,i.jsx)("h1",{style:o.title,children:"Welcome to Phone book"})})}}}]);
-//# sourceMappingURL=266.b76a22ec.chunk.js.map
